1f230d595f1908a0280db7e686a6a80604acea7d
Julien Gil <jgil@student.42.fr>
2020-01-13 19:24:27 +0100

    adding th whole project

---
+++ Assets/Scripts/Analytics/AnalyticsScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using GameAnalyticsSDK;
+using Facebook.Unity;
+
+public class AnalyticsScript : MonoBehaviour {
+    // Buttons
+    public Button eventParamLessButton;
+    public Button eventWithParamButton;
+    public Button eventWithParamAndValueButton;
+
+    // List of Analytics tools adapters
+    public List<AnalyticsAdapter> analyticsTools;
+
+    void Awake() {
+        analyticsTools = new List<AnalyticsAdapter>();
+    }
+
+    void OnEnable()
+    {
+        // Listener button, hardcoded events
+        eventParamLessButton.onClick.AddListener(() => sendSimpleEvent("Event_parameterless"));
+        eventWithParamButton.onClick.AddListener(() => sendEventWithParam("Event_params", "valeur", 42));
+        eventWithParamAndValueButton.onClick.AddListener(() => sendEventWithParamAndValue("Event_full", "nom", "toto", 12));
+    }
+
+    // Method logging a parameterless event on all the subscribed adapters
+    void sendSimpleEvent(string eventName) {
+        foreach (AnalyticsAdapter s in analyticsTools) {
+            s.sendSimpleEvent(eventName);
+        }
+    }
+
+    // Method logging an event with parameter on all the subscribed adapters
+    void sendEventWithParam(string eventName, string paramName, object paramValue) {
+        foreach (AnalyticsAdapter s in analyticsTools) {
+            s.sendEventWithParam(eventName, paramName, paramValue);
+        }
+    }
+
+    // Method logging an event with parameter and a value on all the subscribed adapters
+    void sendEventWithParamAndValue(string eventName, string paramName, object paramValue, int eventValue) {
+        foreach (AnalyticsAdapter s in analyticsTools) {
+            s.sendEventWithParamAndValue(eventName, paramName, paramValue, eventValue);
+        }
+    }
+
+    void OnDisable()
+    {
+        //Un-Register Button Events
+        eventParamLessButton.onClick.RemoveAllListeners();
+        eventWithParamButton.onClick.RemoveAllListeners();
+        eventWithParamAndValueButton.onClick.RemoveAllListeners();
+    }
+}
---
+++ Assets/Scripts/Analytics/GameAnalyticsAdapter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using GameAnalyticsSDK;
+
+public class GameAnalyticsAdapter : AnalyticsAdapter {
+    public override void Start()
+    {
+        base.Start();
+        // Initialize GameAnalytics object
+        GameAnalytics.Initialize();
+    }
+
+    // Send a parameterless event through Game Analytics
+    public override void sendSimpleEvent(string eventName) {
+        GameAnalytics.NewDesignEvent(eventName);
+    }
+
+    // Send an event with a parameter through Game Analytics
+    public override void sendEventWithParam(string eventName, string paramName, object paramValue) {
+        GameAnalytics.NewDesignEvent(eventName+":"+paramName+":"+paramValue.ToString());
+    }
+
+    // Send an event with a parameter and a value through Game Analytics
+    public override void sendEventWithParamAndValue(string eventName, string paramName, object paramValue, int eventValue) {
+        GameAnalytics.NewDesignEvent(eventName+":"+paramName+":"+paramValue.ToString(), eventValue);
+    }
+}
